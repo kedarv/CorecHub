ac87ee27e5b3dcc1e8fbaeff68cb65803fa14f04
Kedar <kedar54@gmail.com>
2014-12-12 17:42:40 -0500

    small visual changes

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -2,22 +2,37 @@
 <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet">
 {{HTML::script('//d3js.org/d3.v3.min.js')}}
 {{ HTML::script('js/cal-heatmap.min.js'); }}
-<div id="cal-heatmap"></div>
+<div class="container">
+	<hr/>
+	<div class="text-center">
+		<div id="cal-heatmap"></div>
+	</div>
+	<hr/>
+	<div class="text-center">
+		<div id="punchcard"></div>
+	</div>
+	<hr/>
+</div>
+
 <script type="text/javascript">
 	var cal = new CalHeatMap();
 	cal.init({
 		itemSelector: "#cal-heatmap",
 		domain: "month",
 		subDomain: "day",
+		domainDynamicDimension: false,
+		domainGutter: 0,
 		data: {{$dataJSON['heat']}},
 		start: new Date(2014, 0),
-		cellSize: 15,
+		cellSize: 12,
 		range: 12,
-		legend: [1]
+		legend: [1],
+		label: {
+			position: "bottom",
+			align: "center",
+		}
 	});
 </script>
-<div id="punchcard"></div>
-
 <script>
 var 
   pane_left = 120,
